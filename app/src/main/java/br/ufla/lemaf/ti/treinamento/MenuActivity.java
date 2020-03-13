256e1c94415386763f6149fcc1e6a3ff0bc9c474
Ian <ian.diasbertolucci@gmail.com>
2020-03-13 14:03:01 -0300

    Recurso de toolbar

--- app/src/main/java/br/ufla/lemaf/ti/treinamento/MenuActivity.java
+++ app/src/main/java/br/ufla/lemaf/ti/treinamento/MenuActivity.java
@@ -7,13 +7,22 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        getSupportActionBar().setHomeAsUpIndicator(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.inflateMenu(R.menu.activity_menu);
+        toolbar.setTitle("toolbar");
+        toolbar.setNavigationIcon(R.mipmap.ic_launcher);
+        toolbar.setOnMenuItemClickListener(this);
     }
 
     @Override
@@ -43,10 +52,42 @@ public class MenuActivity extends AppCompatActivity {
             case R.id.menuDeletar:
                 texto = "deletar";
                 break;
+            case android.R.id.home:
+                texto = "icone app";
         }
 
         txtResultado.setText(texto);
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        TextView txtResultado = (TextView) findViewById(R.id.textView2);
+        String texto = "";
+
+        switch (item.getItemId()){
+            case R.id.menuAdd:
+                texto = "add";
+                break;
+            case R.id.menuAjuda:
+                texto = "ajyda";
+                break;
+            case R.id.menuSalvar:
+                texto = "salvar";
+                break;
+            case R.id.menuEditar:
+                texto = "editar";
+                break;
+            case R.id.menuDeletar:
+                texto = "deletar";
+                break;
+            case android.R.id.home:
+                texto = "icone app";
+        }
+
+        txtResultado.setText(texto);
+        return false;
+
     }
 }
